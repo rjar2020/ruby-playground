8c83f41393a34db11833dc31c80af50da0225e90
rjar2020 <rolando.angelini@factorial.co>
2022-12-04 21:11:11 +0100

    Advent of Code 2022 - catching-up day 3 - needs validations

---
+++ advent2022/day3_rucksack.rb
@@ -0,0 +1,30 @@
+require 'set'
+
+DOWNCASE_OFFSET = 96.freeze
+UPCASE_OFFSET = 38.freeze
+
+def is_upcase(letter)
+  /[[:upper:]]/.match(letter) != nil
+end
+
+def calculate_priority(letter)
+  is_upcase(letter)? letter.ord - UPCASE_OFFSET : letter.ord - DOWNCASE_OFFSET
+end
+
+def find_item_type_both_compartiments(firs_half_set, second_half_set)
+  (firs_half_set & second_half_set).first
+end
+
+priority_total = 0
+f = File.open("rucksack.txt", "r")
+f.each_line do |line|
+  first, second = line.chars.each_slice(line.length / 2).map(&:join)
+  priority_total += calculate_priority(
+    find_item_type_both_compartiments(
+      first.chars.to_set, 
+      second.chars.to_set)
+    )
+end
+f.close
+
+puts priority_total
